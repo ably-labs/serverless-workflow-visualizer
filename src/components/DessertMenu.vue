bbec4333fdb18d94d138b0939c544485b18f800a
Marc Duiker <marcduiker@gmail.com>
2022-09-06 17:09:17 +0200

    Update menu descriptions

--- src/components/DessertMenu.vue
+++ src/components/DessertMenu.vue
@@ -7,18 +7,27 @@ import FlagIcon from "./icons/FlagIcon.vue";
     <h2><FlagIcon /> Dessert menu <FlagIcon /></h2>
     <ul>
       <li>
-        <a href="https://github.com/ably-labs/serverless-workflow-visualizer">
-          GitHub Repo
-        </a>
+        <a href="https://github.com/ably-labs/serverless-workflow-visualizer">GitHub Repo</a>
+        (the star of our desserts ⭐)
       </li>
       <li>
         <a href="https://ably.com/blog">Blog post</a>
+        (our most read menu item 📖)
       </li>
       <li>
         <a href="https://www.youtube.com/c/AblyRealtime">YouTube</a>
+        (please eat with your thumbs up 👍)
       </li>
-      <li><a href="https://ably.com/">Ably website</a></li>
+      <li>
+        <a href="http://go.ably.com/discord">Discord</a>
+        (ideal for sharing 👥)
+      </li>
+      <li>
+        <a href="https://ably.com/">Ably website</a>
+        (hungry for more? 🍴🌍)
+      </li>
     </ul>
+    <p>Allergies should be </p>
   </div>
 </template>
 

66976c74a2a376f009f26ec0dbccee0104fc10f7
Marc Duiker <marcduiker@gmail.com>
2022-09-07 11:35:33 +0200

    Center on small screens and add pineapple cursor

--- src/components/DessertMenu.vue
+++ src/components/DessertMenu.vue
@@ -4,34 +4,40 @@ import FlagIcon from "./icons/FlagIcon.vue";
 
 <template>
   <div class="dessertmenu">
-    <h2><FlagIcon />
+    <h2>
+      <FlagIcon />
       <span class="title word-1"> Dessert </span>
       <span class="title word-2"> menu </span><FlagIcon />
     </h2>
-    <ul>
-      <li>
-        <a href="https://github.com/ably-labs/serverless-workflow-visualizer">GitHub Repo</a>
-        (the star of our desserts ⭐)
-      </li>
-      <li>
-        <a href="https://ably.com/blog">Blog post</a>
-        (our most read menu item 📖)
-      </li>
-      <li>
-        <a href="https://www.youtube.com/c/AblyRealtime">YouTube</a>
-        (please eat with your thumbs up 👍)
-      </li>
-      <li>
-        <a href="http://go.ably.com/discord">Discord</a>
-        (ideal for sharing 👥)
-      </li>
-      <li>
-        <a href="https://ably.com/">Ably website</a>
-        (hungry for more? 🍴🌍)
-      </li>
-    </ul>
+    <div class="flex-list">
+      <ul>
+        <li>
+          <a href="https://github.com/ably-labs/serverless-workflow-visualizer"
+            >GitHub Repo</a
+          >
+          (the star of our desserts ⭐)
+        </li>
+        <li>
+          <a href="https://ably.com/blog">Blog post</a>
+          (our most read menu item 📖)
+        </li>
+        <li>
+          <a href="https://www.youtube.com/c/AblyRealtime">YouTube</a>
+          (please eat with your thumbs up 👍)
+        </li>
+        <li>
+          <a href="http://go.ably.com/discord">Discord</a>
+          (ideal for sharing 👥)
+        </li>
+        <li>
+          <a href="https://ably.com/">Ably website</a>
+          (hungry for more? 🍴🌍)
+        </li>
+      </ul>
+    </div>
     <p>
-      Contact <a href="https://twitter.com/marcduiker">the chef</a> 🧑‍🍳 for additional recipes.
+      Contact <a href="https://twitter.com/marcduiker">the chef</a> 🧑‍🍳 for
+      additional recipes.
     </p>
   </div>
 </template>
@@ -62,5 +68,23 @@ ul {
 
 li {
   margin-bottom: 0.5rem;
+}
+
+h2,
+p {
+  text-align: center;
+}
+
+.flex-list {
+  display: flex;
+  justify-content: center;
+  text-align: left;
+}
+
+@media (min-width: 1024px) {
+  h2,
+  p {
+    text-align: left;
+  }
 }
 </style>

4813b2fc8f9d464a9765c2e6207bf5e6797ca032
Marc Duiker <marcduiker@gmail.com>
2022-09-08 15:08:37 +0200

    Add types

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,5 +1,7 @@
 import { ref, computed } from "vue";
 import { defineStore } from "pinia";
+import type { Types } from "ably";
+import { Realtime } from "ably/promises";
 import type { PizzaWorkflow } from "@/types/PizzaWorkflow";
 import OrderImage from "../assets/Order.png";
 import PizzaAndDrinkImage from "../assets/PizzaAndDrink.png";
@@ -46,5 +48,85 @@ export const pizzaProcessStore = defineStore("pizza-process", {
       isDisabled: true,
     },
   }),
-actions: {}
+  actions: {
+    async createRealtimeConnection(clientId: string, orderID: string) {
+      if (!this.isConnected) {
+        const realtimeClient = new Realtime.Promise({
+          authUrl: `/api/CreateTokenRequest/${clientId}`,
+          echoMessages: false,
+        });
+        this.realtimeClient = realtimeClient;
+        realtimeClient.connection.on(
+          "connected",
+          async (message: Types.ConnectionStateChange) => {
+            this.isConnected = true;
+            await this.attachToChannel(orderID);
+          }
+        );
+
+        this.realtimeClient.connection.on("disconnected", () => {
+          this.isConnected = false;
+        });
+        this.realtimeClient.connection.on("closed", () => {
+          this.isConnected = false;
+        });
+      }
+    },
+    disconnect() {
+      this.realtimeClient?.connection.close();
+    },
+    async attachToChannel(channelName: string) {
+      const channelInstance = this.realtimeClient?.channels.get(channelName);
+      this.channelInstance = channelInstance;
+      this.subscribeToMessages();
+    },
+
+    subscribeToMessages() {
+      this.channelInstance?.subscribe(
+        "receive-order",
+        (message: Types.Message) => {
+          this.handleOrderReceived(message);
+        }
+      );
+      this.channelInstance?.subscribe(
+        "send-instructions-to-kitchen",
+        (message: Types.Message) => {
+          this.handleSendInstructions(message);
+        }
+      );
+      this.channelInstance?.subscribe(
+        "prepare-pizza",
+        (message: Types.Message) => {
+          this.handlePreparePizza(message);
+        }
+      );
+      this.channelInstance?.subscribe(
+        "collect-order",
+        (message: Types.Message) => {
+          this.handleCollectOrder(message);
+        }
+      );
+      this.channelInstance?.subscribe(
+        "deliver-order",
+        (message: Types.Message) => {
+          this.handleDeliverOrder(message);
+        }
+      );
+    },
+    handleOrderReceived(message: Types.Message) {
+      // TODO
+    },
+    handleSendInstructions(message: Types.Message) {
+      // TODO
+    },
+    handlePreparePizza(message: Types.Message) {
+      // TODO
+    },
+    handleCollectOrder(message: Types.Message) {
+      // TODO
+    },
+    handleDeliverOrder(message: Types.Message) {
+      // TODO
+    },
+  },
 });
